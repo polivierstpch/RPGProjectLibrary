6b0ef7475a26015390e1ecff0d83dcc74944929c
Pier-Olivier St-Pierre-Chouinard <pier.olivier.stpch@gmail.com>
2020-07-02 19:15:29 -0400

    Changed Id setter to private in BaseItem class.

--- Models/BaseItem.cs
+++ Models/BaseItem.cs
@@ -7,7 +7,7 @@ namespace RPGProjectLibrary.Models
     {
         private static int _currentId;
         public BaseItem() => Id = ++_currentId;
-        public int Id { get; set; }
+        public int Id { get; private set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public HashSet<ItemProperty> Properties { get; set; }
